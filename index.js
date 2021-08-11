30d6af5c5680171f7463ca9c5b218b880443729b
Mustafa <mchambqa@gmail.com>
2021-08-11 00:03:16 -0400

    commit tree 771bbfb65e9607ee6161ee78bd0e1d3fbd846f05

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function superbowlWin(record){
+    const found = record.find(log => {
+        if(log.result === "W"){
+            return log;
+        }
+    })
+    if(found){
+      return found.year;
+    }else{
+      return found
+    }
+}
